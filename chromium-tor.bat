2422433feb674bb85c93215e1b9a04e46fd9dad8
Alan Sarkar <55506052+alansarkar@users.noreply.github.com>
2022-01-30 14:06:25 +0000

    Tor script updated

--- chromium-tor.bat
+++ chromium-tor.bat
@@ -1,15 +1,19 @@
 @echo off 
-start cmd.exe   .\chromium\exten\tor.exe
-timeout /t 30
+set AutoHDREnable=0
+set GpuPreference=0
 
-set AutoHDREnable=1
-set GpuPreference=1
-start .\chromium\chromium.exe --user-data-dir=./data  ^
+start   .\chromium\exten\tor.exe
+timeout /t 15
+
+
+ .\chromium\chromium.exe --user-data-dir=./data  ^
  --disable-3d-apis --no-pings --disable-renderer-backgrounding  ^
  --disable-extensions-http-throttling  ^
 --force-devtools-available  --disable-background-networking  ^
 --no-wifi --allow-legacy-extension-manifests   ^
 --disable-extensions-http-throttling   --disable-component-extensions-with-background-page ^
 --disable-gl-extensions --disable-dwm-composition --disable-direct-composition --enable-wifi ^
+--user-agent="Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0" ^
 --proxy-server="socks5://127.0.0.1:9050"
 
+rem --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/83.0.4103.116 Safari/537.36"
